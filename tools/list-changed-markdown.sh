c6d60a1eee8ffc6080abd5328d846b9ef3c66f49
yoskeoka <yoske95@gmail.com>
2026-05-29 13:10:57 +0900

    chore: widen Japanese markdown detection

--- tools/list-changed-markdown.sh
+++ tools/list-changed-markdown.sh
@@ -15,9 +15,11 @@ for char in content:
     codepoint = ord(char)
     if (
         0x3040 <= codepoint <= 0x30FF
+        or 0x31F0 <= codepoint <= 0x31FF
         or 0x3400 <= codepoint <= 0x4DBF
         or 0x4E00 <= codepoint <= 0x9FFF
         or 0xF900 <= codepoint <= 0xFAFF
+        or 0xFF65 <= codepoint <= 0xFF9F
     ):
         raise SystemExit(0)
 raise SystemExit(1)
